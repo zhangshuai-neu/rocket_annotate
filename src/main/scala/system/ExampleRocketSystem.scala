18fc1deb18003872b37f8c5dc7b294c098bc8e6b
ckj <ckj119940887@gmail.com>
2019-04-17 11:39:22 -0400

    Changes to be committed: 	modified:   .idea/workspace.xml 	modified:   src/main/scala/system/ExampleRocketSystem.scala 	modified:   src/main/scala/system/TestHarness.scala

--- src/main/scala/system/ExampleRocketSystem.scala
+++ src/main/scala/system/ExampleRocketSystem.scala
@@ -10,13 +10,16 @@ import freechips.rocketchip.subsystem._
 import freechips.rocketchip.devices.tilelink._
 import freechips.rocketchip.util.DontTouch
 
+//ExampleRocketSystem是在RocketSubsystem的基础上进行扩展的。
+//RocketSubsystem是在/subsystem/RocketSubsystem.scala中定义的类。
 /** Example Top with periphery devices and ports, and a Rocket subsystem */
 class ExampleRocketSystem(implicit p: Parameters) extends RocketSubsystem
-    with HasAsyncExtInterrupts
+    with HasAsyncExtInterrupts  //将Ext-Interrupts(PLIC的中断源)引入ibus中同步，即异步信号同步化。
+                                //在/subsystem/InterruptBus.scala中定义。
     with CanHaveMasterAXI4MemPort
     with CanHaveMasterAXI4MMIOPort
     with CanHaveSlaveAXI4Port
-    with HasPeripheryBootROM {
+    with HasPeripheryBootROM {  //调用BootROM模块。在/devices/tilelink/BootROM.scala中定义。
   override lazy val module = new ExampleRocketSystemModuleImp(this)
 
   // The sbus masters the cbus; here we convert TL-UH -> TL-UL
@@ -39,11 +42,14 @@ class ExampleRocketSystem(implicit p: Parameters) extends RocketSubsystem
   }
 }
 
+//具体的端口连接模块
+//RocketSubsystemModuleImp是在/subsystem/RocketSubsystem.scala中定义的类
 class ExampleRocketSystemModuleImp[+L <: ExampleRocketSystem](_outer: L) extends RocketSubsystemModuleImp(_outer)
-    with HasRTCModuleImp
-    with HasExtInterruptsModuleImp
-    with CanHaveMasterAXI4MemPortModuleImp
-    with CanHaveMasterAXI4MMIOPortModuleImp
-    with CanHaveSlaveAXI4PortModuleImp
-    with HasPeripheryBootROMModuleImp
+    with HasRTCModuleImp  //用于连接CLINT timer的时钟输入，可以直接将rtc_clk引到顶层中。在/subsystem/RTC.scala中定义。
+    with HasExtInterruptsModuleImp  //将顶层的中断源(Ext-Interrupts)信号接到ExtInterruptsModule中。在/subsystem/InterruptBus.scala中定义。
+    with CanHaveMasterAXI4MemPortModuleImp  //将AXI4协议的SRAM和DUT(ExampleRocketSystem)的memory_port总线(AXI4协议)相连。在/subsystem/Ports.scala中定义。
+    with CanHaveMasterAXI4MMIOPortModuleImp //将AXI4协议的SRAM和DUT(ExampleRocketSystem)的mmio_port总线(AXI4协议)相连。在/subsystem/Ports.scala中定义。
+    with CanHaveSlaveAXI4PortModuleImp  //将顶层的frontend_bus信号(AXI4协议)接到SlaveAXI4PortModule中。在/subsystem/Ports.scala中定义。
+    with HasPeripheryBootROMModuleImp //将global_reset_vector接着BootROM的地址上，固定rocket-chip从BootROM开始启动。
+                                      //在/devices/tilelink/BootROM.scala中定义。
     with DontTouch
--- src/main/scala/system/TestHarness.scala
+++ src/main/scala/system/TestHarness.scala
@@ -12,13 +12,16 @@ class TestHarness()(implicit p: Parameters) extends Module {
     val success = Bool(OUTPUT)
   }
 
+  //注释：dut的复位是外部reset与dut debug模块的ndreset信号的或。
   val dut = Module(LazyModule(new ExampleRocketSystem).module)
   dut.reset := reset | dut.debug.ndreset
 
   dut.dontTouchPorts()
-  dut.tieOffInterrupts()
-  dut.connectSimAXIMem()
+  dut.tieOffInterrupts()  //这个是将dut的中断信号全部接0
+  dut.connectSimAXIMem()  //这些是原有的AXI接口，Mem和MMIO,在/subsystem/Ports.scala中定义。
   dut.connectSimAXIMMIO()
-  dut.l2_frontend_bus_axi4.foreach(_.tieoff)
-  Debug.connectDebug(dut.debug, clock, reset, io.success)
+  dut.l2_frontend_bus_axi4.foreach(_.tieoff)  //这是frontend的接口，这个接口CPU是作为slave的，外部设备是作为master的,功能类似于SiFive的frontport，采用AXI4协议，
+  Debug.connectDebug(dut.debug, clock, reset, io.success)   //注释：主要作用是将SimJTAG.v与dut相连，并输出io.success来确定仿真是否成功。
+                                                            //注释：SimJTAG.v在/rocket-chip/vsim/generated-src/xxx目录中可以找到。
+                                                            //注释：在/devices/debug/Periphery.scala中定义。
 }
